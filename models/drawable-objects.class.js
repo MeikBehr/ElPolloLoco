7b3b9b93eb98db62edfc2cbf79e91f79666ffe14
Meik Behr <meik.behr@gmx.de>
2024-11-02 12:04:43 +0100

    Cleancoding & commenting done and tested: drawable-objects.class.js

--- models/drawable-objects.class.js
+++ models/drawable-objects.class.js
@@ -1,3 +1,6 @@
+/**
+ * Base class for drawable objects in the game.
+ */
 class DrawableObjects {
     x;
     y;
@@ -7,32 +10,39 @@ class DrawableObjects {
     currentImage = 0;
     img;
 
-
     /**
-     * 
-     * @param {Array} arr - ['img/image1.png', 'img/image2.png', ....]
+     * Loads an array of images into the image cache.
+     * @param {Array<string>} arr - Array of image paths to load, e.g., ['img/image1.png', 'img/image2.png']
      */
     loadImages(arr) {
         arr.forEach((path) => {
             let img = new Image();
             img.src = path;
             this.imageCache[path] = img;
-        })
+        });
     }
 
-    
+    /**
+     * Loads a single image from the specified path.
+     * @param {string} path - The path of the image to load.
+     */
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
 
-    
+    /**
+     * Draws the current image of the object onto the specified canvas context.
+     * @param {CanvasRenderingContext2D} ctx - The canvas rendering context to draw on.
+     */
     draw(ctx) {
         ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     }
 
+    /**
+     * Clears all intervals set in the window.
+     */
     clearAllIntervals() {
         for (let i = 1; i < 9999; i++) window.clearInterval(i);
     }
-
 }
