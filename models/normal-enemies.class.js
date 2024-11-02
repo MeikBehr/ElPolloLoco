623b1f8c59f3a458b3264f4c20f2edb4d1cabb93
Meik Behr <meik.behr@gmx.de>
2024-11-02 12:18:49 +0100

    Cleancoding & commenting done and tested: normal-enemies.class.js

--- models/normal-enemies.class.js
+++ models/normal-enemies.class.js
@@ -1,26 +1,33 @@
-
+/** 
+ * Represents a normal enemy in the game that can move and be animated.
+ * @extends MovableObject
+ */
 class NormalEnemies extends MovableObject {
-
-
+    /** 
+     * Animates the walking animation of the enemy.
+     */
     animationIsWalking() {
         if (!isPaused) {
             this.playAnimation(this.IMAGES_WALKING);    
         }
     }
 
-
+    /** 
+     * Animates the death animation of the enemy.
+     */
     animationIsDead() {
         this.playAnimation(this.IMAGES_DEAD);
     }
 
-
+    /**
+     * Initiates the movement and animation of the enemy.
+     */
     animate() {
-
         setInterval(() => { 
             this.moveLeft();
-        }, 1000 / 60)
+        }, 1000 / 60);
 
-        setInterval( () => {
+        setInterval(() => {
             if (!this.enemyIsDead) {
                 this.animationIsWalking();
             } else {
@@ -29,5 +36,4 @@ class NormalEnemies extends MovableObject {
             }
         }, 175);
     }
-
 }
