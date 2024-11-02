4bac8c0d5fa856b30841a6de632fd8dc2c29f0ed
Meik Behr <meik.behr@gmx.de>
2024-11-02 10:57:32 +0100

    Cleancoding & commenting done and tested:  bottles-throwable.class.js

--- models/bottles-throwable.class.js
+++ models/bottles-throwable.class.js
@@ -1,19 +1,38 @@
 class BottleThrowable extends ThrowableObject {
-
+    /**
+     * Height of the throwable bottle.
+     * @type {number}
+     */
     height = 80;
+
+    /**
+     * Width of the throwable bottle.
+     * @type {number}
+     */
     width = 80;
+
+    /**
+     * Indicates if the bottle is currently thrown.
+     * @type {boolean}
+     */
     isThrown = false;
+
+    /**
+     * Indicates if the bottle is thrown in the opposite direction.
+     * @type {boolean}
+     */
     otherDirection = false;
-    
-    
-    offset = {
-        top: 5,
-        bottom: 10,
-        left: 13,
-        right: 20,
-      };
 
+    /**
+     * Offset values for collision detection.
+     * @type {Object}
+     */
+    offset = { top: 5, bottom: 10, left: 13, right: 20 };
 
+    /**
+     * Images for bottle rotation animation.
+     * @type {Array<string>}
+     */
     IMAGES_BOTTLE_ROTATION = [
         './assets/img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
         './assets/img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
@@ -21,6 +40,10 @@ class BottleThrowable extends ThrowableObject {
         './assets/img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png',
     ];
 
+    /**
+     * Images for bottle splash animation.
+     * @type {Array<string>}
+     */
     IMAGES_BOTTLE_SPLASH = [
         './assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
         './assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
@@ -30,7 +53,12 @@ class BottleThrowable extends ThrowableObject {
         './assets/img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png',
     ];
 
-
+    /**
+     * Initializes a throwable bottle with position and direction.
+     * @param {number} x - Initial X position.
+     * @param {number} y - Initial Y position.
+     * @param {boolean} otherDirection - If true, bottle is thrown in opposite direction.
+     */
     constructor(x, y, otherDirection) {
         super();
         this.loadImages(this.IMAGES_BOTTLE_ROTATION);
@@ -42,23 +70,19 @@ class BottleThrowable extends ThrowableObject {
         this.animate();
     }
 
+    /**
+     * Animates the bottle while in motion, switching to splash animation upon collision.
+     */
     animate() {
-
         if (!isPaused) {
-            let intervalId = setInterval(() => {
+            const intervalId = setInterval(() => {
                 if (this.isAboveGround() && !this.objectIsColiding) {
-                    if(!isPaused) {
-                        this.playAnimation(this.IMAGES_BOTTLE_ROTATION);
-                    }
+                    this.playAnimation(this.IMAGES_BOTTLE_ROTATION);
                 } else {
-                    if(!isPaused) {
-                        this.playAnimation(this.IMAGES_BOTTLE_SPLASH);
-                        clearInterval(intervalId);
-                    }
+                    this.playAnimation(this.IMAGES_BOTTLE_SPLASH);
+                    clearInterval(intervalId);
                 }
             }, 100);
         }
-
     }
-
 }
