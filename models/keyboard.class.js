17a0b4ac6528a9abb2f962c756c414c074b738e3
Meik Behr <meik.behr@gmx.de>
2024-11-02 12:07:37 +0100

    Cleancoding & commenting done and tested: keyboard.class.js

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -1,10 +1,22 @@
+/** 
+ * Represents the keyboard input state for controlling game actions.
+ */
 class Keyboard {
-
+    /** @type {boolean} Indicates if the left arrow key is pressed */
     LEFT = false;
-    RIGHT = false;
-    UP = false;
-    DOWN = false;
-    SPACE = false;
-    D = false;
 
+    /** @type {boolean} Indicates if the right arrow key is pressed */
+    RIGHT = false;
+
+    /** @type {boolean} Indicates if the up arrow key is pressed */
+    UP = false;
+
+    /** @type {boolean} Indicates if the down arrow key is pressed */
+    DOWN = false;
+
+    /** @type {boolean} Indicates if the spacebar is pressed */
+    SPACE = false;
+
+    /** @type {boolean} Indicates if the 'D' key is pressed */
+    D = false;
 }
