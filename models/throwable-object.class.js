23a02a0bde3de156ba6aa3faf4334284b434db87
Meik Behr <meik.behr@gmx.de>
2024-11-02 12:22:55 +0100

    Cleancoding & commenting done and tested: throwable-object.class.js

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,45 +1,69 @@
+/** 
+ * Represents a throwable object in the game, such as a bottle.
+ * @extends MovableObject
+ */
 class ThrowableObject extends MovableObject {
+  /** 
+   * Horizontal speed of the throwable object.
+   * @type {number}
+   */
+  speedX = 8;
 
-    speedX = 8;
-    speedY = 20;
+  /** 
+   * Vertical speed of the throwable object.
+   * @type {number}
+   */
+  speedY = 20;
 
-    constructor(x, y) {
-        super();
-        this.loadImage('./assets/img/6_salsa_bottle/salsa_bottle.png');
-        this.x = x;
-        this.y = y;
-        this.height = 80;
-        this.width = 80;
-    }
+  /** 
+   * Creates an instance of a throwable object at the specified position.
+   * @param {number} x - The x-coordinate of the throwable object.
+   * @param {number} y - The y-coordinate of the throwable object.
+   */
+  constructor(x, y) {
+      super();
+      this.loadImage('./assets/img/6_salsa_bottle/salsa_bottle.png');
+      this.x = x;
+      this.y = y;
+      this.height = 80;
+      this.width = 80;
+  }
 
+  /** 
+   * Offset for collision detection.
+   * @type {Object}
+   * @property {number} top - The top offset.
+   * @property {number} bottom - The bottom offset.
+   * @property {number} left - The left offset.
+   * @property {number} right - The right offset.
+   */
+  offset = {
+      top: 5,
+      bottom: 5,
+      left: 13,
+      right: 13,
+  };
 
-    offset = {
-        top: 5,
-        bottom: 5,
-        left: 13,
-        right: 13,
-    };
-
-
-    throw(x, y, otherDirection) {
-        x = x;
-        y = y;
-        this.applyGravity();
-        if (!isPaused) {
+  /** 
+   * Initiates the throwing action for the throwable object.
+   * @param {number} x - The x-coordinate where the object is thrown.
+   * @param {number} y - The y-coordinate where the object is thrown.
+   * @param {boolean} otherDirection - Indicates the direction of the throw.
+   */
+  throw(x, y, otherDirection) {
+      this.applyGravity();
+      if (!isPaused) {
           setInterval(() => {
               if (otherDirection) {
-                if (!isPaused) {
-                  this.x -=this.speedX;
-                }
+                  if (!isPaused) {
+                      this.x -= this.speedX;
+                  }
               } else {
-                if (!isPaused) {
-                  this.x += this.speedX;
-                }
+                  if (!isPaused) {
+                      this.x += this.speedX;
+                  }
               }
-          }, 25)
-        }
-
-    }
-
-
+          }, 25);
+      }
+  }
 }
